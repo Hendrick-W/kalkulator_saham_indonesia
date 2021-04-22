be329f7e922995578b2cdb7e4be3ac778f79ee7b
Hendrick <hendrickwijaya21@gmail.com>
2021-04-22 19:45:19 +0700

    Adding redux persist and broker info

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -3,14 +3,22 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen'
+import FeeScreen from '../screens/FeeScreen'
+import ProfitScreen from '../screens/ProfitScreen';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown:false
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Fee" component={FeeScreen} />
+        <Stack.Screen name="Profit" component={ProfitScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/FeeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FeeScreen = () => {
+  return (
+    <View>
+      <Text>FeeScreen</Text>
+    </View>
+  )
+}
+
+export default FeeScreen
+
+const styles = StyleSheet.create({})
